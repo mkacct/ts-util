8783b69d69bd9013933be32cb4fee58168dda70f
Madeline Kahn <mk-acct@outlook.com>
2025-04-07 20:26:02 -0400

    Tweak eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,7 @@ export default tseslint.config(
 			"@typescript-eslint/no-explicit-any": "warn",
 			"@typescript-eslint/no-unused-vars": ["warn", {
 				argsIgnorePattern: "^_",
+				destructuredArrayIgnorePattern: "^_"
 			}]
 		}
 	},
